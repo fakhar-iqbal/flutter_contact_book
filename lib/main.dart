08f323c3bf3f557959f73664e73c750f74e66412
Muhammad Fakhar-e-Iqbal Abdullah <fakhareiqbal3534@gmail.com>
2024-09-16 02:58:34 +0500

    state for value

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,35 +17,40 @@ void main() {
 }
 
 class Contact {
+  final String id;
   final String name;
 
-  Contact({required this.name});
+  Contact({required this.name}) : id = const Uuid().v4();
 }
 
-class ContactBook {
-  ContactBook._sharedInstance();
+class ContactBook extends ValueNotifier<List<Contact>> {
+  ContactBook._sharedInstance() : super([]);
   static final ContactBook _shared = ContactBook._sharedInstance();
 
   factory ContactBook() => _shared;
 
   //creating a list to contain all the contacts
 
-  final List<Contact> _contacts = [];
-
-  int get length => _contacts.length;
+  int get length => value.length;
 
   void add({required Contact contact}) {
-    _contacts.add(contact);
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
   }
 
   void remove({required Contact contact}) {
-    _contacts.remove(contact);
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
   }
 
   // getting contacts at paeticular index
 
   Contact? contact({required int atIndex}) =>
-      atIndex < _contacts.length ? _contacts[atIndex] : null;
+      atIndex < value.length ? value[atIndex] : null;
 }
 
 class HomePage extends StatelessWidget {
@@ -56,18 +62,35 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contact Book'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-        itemCount: contactBook.length,
-        itemBuilder: (context, index) {
-          final contact = contactBook.contact(atIndex: index)!;
-          return ListTile(
-            title: Text(
-              contact.name,
-            ),
-          );
-        },
+      body: ValueListenableBuilder(
+        valueListenable: ContactBook(),
+        builder:(contact, value,  child){
+          final contacts = value as List<Contact>;
+          return ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            final contact = contacts[index];
+            return Dismissible(
+              onDismissed: (direction){
+                ContactBook().remove(contact: contact);
+              },
+              key: ValueKey(contact.id),
+              child: Material(
+                color: Colors.white,
+                elevation: 6.0,
+                child: ListTile(
+                  title: Text(
+                    contact.name,
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+        }
+        
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -75,7 +98,7 @@ class HomePage extends StatelessWidget {
         },
         child: const Icon(
           Icons.add,
-          color: Colors.blueAccent,
+          color: Colors.blue,
         ),
       ),
     );
@@ -109,7 +132,7 @@ class _NewContactState extends State<NewContact> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add New contact'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.blue,
       ),
       body: Column(
         children: [
@@ -133,7 +156,7 @@ class _NewContactState extends State<NewContact> {
         },
         child: const Icon(
           Icons.done,
-          color: Colors.blueAccent, 
+          color: Colors.blueAccent,
         ),
       ),
     );
